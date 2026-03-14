79d28d6f52c206e4126875d248e0ce96ca67c2a3
DARCKISON A TRAJANO <147194583+Darckison@users.noreply.github.com>
2026-03-14 03:41:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import LandingPage from './pages/LandingPage';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
 import NovoMembro from './pages/NovoMembro';
-import UserRanking from './pages/UserRanking'; // Ranking Geral
-import UnitRanking from './pages/UnitRanking'; // Ranking por Unidade
-import GerenciarUnidades from './pages/GerenciarUnidades';//Gerenciar as unidades dbv
+import UserRanking from './pages/UserRanking'; 
+import UnitRanking from './pages/UnitRanking'; 
+import GerenciarUnidades from './pages/GerenciarUnidades';
+import Chamada from './pages/Chamada'; 
 
 function App() {
   return (
@@ -16,14 +17,13 @@ function App() {
         <Route path="/login" element={<AdminLogin />} />
         <Route path="/dashboard" element={<AdminDashboard />} />
         <Route path="/novo-membro" element={<NovoMembro />} />
-        <Route path="/ranking" element={<UserRanking />} /> {/* Rota para os usuários verem o placar */}
-        <Route path="/ranking-unidades" element={<UnitRanking />} /> {/* Rota para o ranking por unidades */}
-        <Route path="/admin-unidades" element={<GerenciarUnidades />} />{/* Rota para gerenciar Unidades */}
+        <Route path="/ranking" element={<UserRanking />} /> 
+        <Route path="/ranking-unidades" element={<UnitRanking />} /> 
+        <Route path="/admin-unidades" element={<GerenciarUnidades />} />
+        <Route path="/chamada" element={<Chamada />} />
       </Routes>
     </Router>
   );
 }
 
-
 export default App;
-
